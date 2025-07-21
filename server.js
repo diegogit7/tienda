2d844696c22b1d3e92f2bc947fc1c23d278c1279
Diego Zerene <zerenescos@gmail.com>
2025-07-21 19:18:12 -0400

    g

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ console.log("Iniciando backend Mercado Pago...");
 const express = require('express');
 const cors = require('cors');
 
-console.log("Antes de importar MercadoPago");
-const { MercadoPagoConfig, Preference } = require('mercadopago');
-console.log("Después de importar MercadoPago");
+//console.log("Antes de importar MercadoPago");
+//const { MercadoPagoConfig, Preference } = require('mercadopago');
+//console.log("Después de importar MercadoPago");
 
 const path = require('path');
 
@@ -18,46 +18,46 @@ app.use(express.static(path.join(__dirname)));
 console.log("Middlewares cargados");
 
 
-console.log("Valor del token:", process.env.MERCADOPAGO_TOKEN);
+//console.log("Valor del token:", process.env.MERCADOPAGO_TOKEN);
 
-let client;
-try {
-  client = new MercadoPagoConfig({
-    accessToken: process.env.MERCADOPAGO_TOKEN
-  });
-  console.log("Cliente Mercado Pago creado:", !!client);
-} catch (err) {
-  console.error("Error creando cliente Mercado Pago:", err);
-}
+//let client;
+//try {
+//  client = new MercadoPagoConfig({
+  //  accessToken: process.env.MERCADOPAGO_TOKEN
+//  });
+ // console.log("Cliente Mercado Pago creado:", !!client);
+//} catch (err) {
+ // console.error("Error creando cliente Mercado Pago:", err);
+//}
 
-app.post('/crear-preferencia', async (req, res) => {
-  try {
-    const items = req.body.items.map(item => ({
-      title: item.title,
-      quantity: item.quantity,
-      unit_price: item.unit_price,
-      currency_id: "CLP"
-    }));
-    console.log("Recibido en backend:", items);
+//app.post('/crear-preferencia', async (req, res) => {
+ // try {
+    //const items = req.body.items.map(item => ({
+     // title: item.title,
+     // quantity: item.quantity,
+     // unit_price: item.unit_price,
+     // currency_id: "CLP"
+   // }));
+   // console.log("Recibido en backend:", items);
 
-    const preference = new Preference(client);
-    const result = await preference.create({
-      body: {
-        items: items,
-        back_urls: {
-          success: "https://tusitio.com/success",
-          failure: "https://tusitio.com/failure",
-          pending: "https://tusitio.com/pending"
-        },
-        auto_return: "approved"
-      }
-    });
-    res.json({ id: result.id });
-  } catch (error) {
-    console.error("Error en /crear-preferencia:", error);
-    res.status(500).json({ error: error.message });
-  }
-});
+    //const preference = new Preference(client);
+    //const result = await preference.create({
+     // body: {
+        //items: items,
+       // back_urls: {
+          //success: "https://tusitio.com/success",
+         // failure: "https://tusitio.com/failure",
+         // pending: "https://tusitio.com/pending"
+        //},
+       // auto_return: "approved"
+     // }
+   // });
+   // res.json({ id: result.id });
+ // } catch (error) {
+    //console.error("Error en /crear-preferencia:", error);
+  //  res.status(500).json({ error: error.message });
+ // }
+//});
 
 app.get('/', (req, res) => {
   res.send('Servidor funcionando');
