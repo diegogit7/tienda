0134ce30b92bca7f56987cb00d66f954544a2935
Diego Zerene <zerenescos@gmail.com>
2025-07-21 18:02:24 -0400

    3

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.get('/', (req, res) => {
   res.send('Servidor funcionando');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Servidor Mercado Pago en puerto ${PORT}`));
